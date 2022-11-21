931fd8bb27bd7ba0a3d3410da2027addeb14c823
unknown <shannon.carson@helsinki.fi>
2022-11-21 23:57:52 +0200

    upload assig 3 test 1

--- data/create_alc.R
+++ data/create_alc.R
@@ -4,8 +4,11 @@
 
 # Script to read in, clean and select variables of interest to put into new data set
 
-# This data approach student achievement in secondary education of two Portuguese schools. 
-# The data attributes include student grades, demographic, social and school related features) and it was collected by using school reports and questionnaires. Two datasets are provided regarding the performance in two distinct subjects: Mathematics (mat) and Portuguese language (por).
+"This data approach student achievement in secondary education of two Portuguese schools.
+The data attributes include student grades, demographic, social and school related features)
+and it was collected by using school reports and questionnaires. Two datasets are provided
+regarding the performance in two distinct subjects: Mathematics (mat) and Portuguese language (por)."
+
 # Available at: https://archive.ics.uci.edu/ml/datasets/Student+Performance"
 
 #Relevant packages and libraries 
@@ -16,28 +19,69 @@ library(dplyr)
 
 # read in data 
 
-student_mat <- read.csv()
+student_mat <- read.csv("student-mat.csv")
 
-student_por <- 
+student_por <- read.csv("student-por.csv")
 
+# explore structure and dimensions of the data
+str(student_mat)
+dim(student_mat)
+
+str(student_por)
+dim(student_por)
 
 # Task 2 -----------------------------------------------------------------------
 
+# columns that vary in the two data sets
+free_cols <- c("failures", "paid", "absences", "G1", "G2","G3")
+
+# common identifiers used for joining the data sets
+join_cols <- setdiff(colnames(por), free_cols)
+
+# join the two data sets by the selected identifiers
+math_por <- inner_join(math, por, by = join_cols, suffix = c(".math", ".por"))
+
+# explore structure and dimensions of the data
+str(math_por)
+dim(math_por)
+
 # Task 3 -----------------------------------------------------------------------
+
+# create a new data frame with only the joined columns
+alc <- select(math_por, all_of(join_cols))
+
+# for every column name not used for joining...
+for(col_name in free_cols) {
+  # select two columns from 'math_por' with the same original name
+  two_cols <- select(math_por, starts_with(col_name))
+  # select the first column vector of those two columns
+  first_col <- select(two_cols, 1)[[1]]
+  
+  # then, enter the if-else structure!
+  # if that first column vector is numeric...
+  if(is.numeric(first_col)) {
+    # take a rounded average of each row of the twous columns and
+    # add the resulting vector to the alc data frame
+    alc[col_name] <- round(rowMeans(two_cols))
+  } else { # else (if the first column vector was not numeric)...
+    # add the first column vector to the alc data frame
+    alc[col_name] <- first_col
+  }
+}
 
 # Task 4 -----------------------------------------------------------------------
 
+# create new column combining weekday and weekend alcohol use
+alc <- mutate(alc, alc_use = (Dalc + Walc) / 2)
+
+# create new binary column realted to high alcohol use
+alc <- mutate(alc, high_use = alc_use > 2)
+
 # Task 5 -----------------------------------------------------------------------
 
+# check data to ensure correct 
+glimpse(alc)
 
-#Create a new R script with RStudio. Write your name, date and a one sentence file description as a comment on the top of the script (include a reference to the data source). Save the script as 'create_alc.R' in the ‘data’ folder of your project. Complete the rest of the steps in that script.
-#
-#Read both student-mat.csv and student-por.csv into R (from the data folder) and explore the structure and dimensions of the data. (1 point)
-#
-#Join the two data sets using all other variables than "failures", "paid", "absences", "G1", "G2", "G3" as (student) identifiers. Keep only the students present in both data sets. Explore the structure and dimensions of the joined data. (1 point)
-#
-#Get rid of the duplicate records in the joined data set. Either a) copy the solution from the exercise "3.3 The if-else structure" to combine the 'duplicated' answers in the joined data, or b) write your own solution to achieve this task. (1 point)
-#
-#Take the average of the answers related to weekday and weekend alcohol consumption to create a new column 'alc_use' to the joined data. Then use 'alc_use' to create a new logical column 'high_use' which is TRUE for students for which 'alc_use' is greater than 2 (and FALSE otherwise). (1 point)
-#
-#Glimpse at the joined and modified data to make sure everything is in order. The joined data should now have 370 observations. Save the joined and modified data set to the ‘data’ folder, using for example write_csv() function (readr package, part of tidyverse). (1 point)
+# write created dataset to csv 
+write.csv(alc, "student_alc_use.csv", row.names=FALSE)
+
