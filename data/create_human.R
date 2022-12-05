59fb5ad019ed9a0c079ad38a7a05a5a757b9629d
unknown <shannon.carson@helsinki.fi>
2022-12-05 21:50:16 +0200

    data wrangling completion

--- data/create_human.R
+++ data/create_human.R
@@ -1,5 +1,5 @@
 # author: Emilia Carson 
-# date: November 27 2022 
+# date: December 5th 2022 
 
 # Script to read in, clean and create new variables of interest and join into new data set
 
@@ -14,6 +14,8 @@ from most countries in the world."
 
 #Relevant packages and libraries 
 library(tidyverse)
+library(stringr)
+library(tibble)
 
 # TASK 1------------------------------------------------------------------------
 
@@ -64,7 +66,44 @@ gii$Edu2.FM = gii$Edu2.F/gii$Edu2.M
 # variable of ratio of Male and Female populations with secondary education in each country
 gii$Edu2.MF = gii$Edu2.M/gii$Edu2.F
 
+# variable for ratio if female and male population in the labour force 
+gii$Labo.FM = gii$Labo.F/gii$Labo.M 
+
 # TASK 4------------------------------------------------------------------------
 
 # join together the two data sets 
 human <- inner_join(hd, gii, by = "Country")
+
+# TASK 5------------------------------------------------------------------------
+
+# remove the commas from GNI and print out a numeric version of it
+str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric()
+
+# TASK 6------------------------------------------------------------------------
+
+# remove uneeded variables 
+human_small <- human[ , ! names(human) %in% c("HDI_Rank","HDI", "Edu.Mean",
+                                              "GNI.Minus.Rank","GII_Rank","GII",
+                                              "Edu2.F","Edu2.M","Edu2.MF",
+                                              "Labo.M", "GNI_adj", "Labo.F")]
+
+# TASK 7------------------------------------------------------------------------
+
+# remove all rows with missing values 
+human_small <- filter(human_small, complete.cases(human_small))
+
+# TASK 8------------------------------------------------------------------------
+
+# remove observations related to regions 
+last <- nrow(human_small) - 7
+
+# TASK 9------------------------------------------------------------------------
+
+# choose everything until the last 7 observations
+human_clean <- human_small[1:last,]
+
+# add countries as rownames
+human_clean <- column_to_rownames(human_clean, var = 'Country')
+
+# save data (RDS format to keep row names)
+saveRDS(human_clean, "human.rds")
